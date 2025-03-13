83b449ace8ed05e3a7586bdfff96afd22ebf3cea
Alex <alexander.vmk@gmail.com>
2025-03-13 08:26:09 +0300

    experimenting with pdfbox

---
+++ src/test/java/hipravin/jarvis/bookstore/PdfBoxPlaygroundIT.java
@@ -0,0 +1,51 @@
+package hipravin.jarvis.bookstore;
+
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.io.RandomAccessReadBufferedFile;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class PdfBoxPlaygroundIT {
+
+    @Test
+    void sampleReadPdf() throws IOException {
+        String samplePdfFile = "src/test/resources/data/bookstore/garlic-onion-15.JChromat.A2006.pdf";
+        assertTrue(Files.isRegularFile(Path.of(samplePdfFile)));
+
+        try (PDDocument document = Loader.loadPDF(new RandomAccessReadBufferedFile(samplePdfFile))) {
+
+            PDFTextStripper stripper = new PDFTextStripper();
+
+            stripper.setStartPage(21);
+            stripper.setEndPage(21);
+            String text = stripper.getText(document);
+
+            document.removePage(20);
+            document.removePage(19);
+            document.removePage(18);
+
+
+            System.out.println(text);
+
+
+//            for (PDPage page : document.getPages()) {
+//
+//                String contents = new String(page.getContents().readAllBytes());
+//
+//                System.out.println("===== PAGE " + page.getAnnotations() +"  ===========");
+//
+//                System.out.println(contents);
+//
+//                System.out.println("=======================");
+//            }
+        }
+
+    }
+}
